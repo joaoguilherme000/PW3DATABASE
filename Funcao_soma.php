72261cef5f25687482c9213c78a7b3299b46dfb5
joao guilherme <joaoguilhermehig@gmail.com>
2023-09-04 19:58:14 -0300

    FUNCTION COLOCADO

---
+++ Funcao_soma.php
@@ -0,0 +1,14 @@
+<?php
+    function somaValor (){
+        $num1=2;
+        $num2=2;
+        $resp=$num1+$num2;
+
+        echo "O valor da soma é $resp";
+      }
+
+      somaValor();
+
+?>
+
+
