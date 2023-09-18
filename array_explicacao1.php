cd3e4b9e64735557ff6edf813ed3249d347602ee
joaoguilherme <joaoguilhermehig@gmail.com>
2023-09-18 19:24:13 -0300

    mostrar todos os valores dentro do vetor com um foreach

--- array_explicacao1.php
+++ array_explicacao1.php
@@ -2,6 +2,10 @@
 
 $nome = array('etecia', 10, true, 45.50);
 
+foreach ($nome as $nomes) {
+    echo " Valores : " .  $nomes. "<br>";
+}
+
 for ($i = 0; $i < count($nome); $i++) {
     echo "O valor: " . $nome[$i] . "<br>";
 }
