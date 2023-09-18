ea0c8de4292ae48ac6e8957f7ba6b63984d3d9e5
joaoguilherme <joaoguilhermehig@gmail.com>
2023-09-18 19:40:02 -0300

    atividade 2

---
+++ array_explicacao3.php
@@ -0,0 +1,18 @@
+<?php
+
+    $num = array(1,2,3,4,5);
+
+    foreach ($num as $valor) {
+        echo "Valor é $valor <br/>";
+    }
+
+    $num[0] = "um";
+    $num[1] = "dois";
+    $num[2] = "tres";
+    $num[3] = "quatro";
+    $num[4] = "cinco";
+
+    foreach ($num as $valor) {
+        echo "<br/> Valor é $valor <br/>";
+    }
+?>
