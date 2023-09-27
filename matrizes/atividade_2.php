d4f5a13bd32bd2ae0a04d4c982a49fbba5d78288
joaoguilherme <joaoguilhermehig@gmail.com>
2023-09-27 19:15:24 -0300

    matriz D criada

--- matrizes/atividade_2.php
+++ matrizes/atividade_2.php
@@ -1,6 +1,6 @@
 <?php
 
- $m = array(
+ $b = array(
     array(25,12,35),
     array(85,47,98),
     array(32,38,105)
@@ -12,13 +12,38 @@
     array(74,14,3)
  );
 
+$D = array();
+
     for ($L=0; $L < 3; $L++) { 
         for ($C=0; $C < 3; $C++) { 
-            echo $m[$L][$C] + $a[$L][$C] . " &nbsp;|&nbsp; ";
+            echo $a[$L][$C] . " &nbsp;|&nbsp; ";
         }
         echo"<br/>";
     }
 
+    echo "<br>VALOR DE CIMA DA MATRIZ A <br><br>";
+
+    for ($L=0; $L < 3; $L++) { 
+        for ($C=0; $C < 3; $C++) { 
+            echo $b[$L][$C] . " &nbsp;|&nbsp; ";
+        }
+        echo"<br/>";
+    }
+
+    echo "<br>VALOR DE CIMA DA MATRIZ B <br><br>";
+
+    for ($L=0; $L < 3; $L++) { 
+        for ($C=0; $C < 3; $C++) { 
+            $D[$L][$C] = $b[$L][$C] + $a[$L][$C];
+            echo $b[$L][$C] + $a[$L][$C] . " &nbsp;|&nbsp; ";
+            
+        }
+        echo"<br/>";
+    }
+
+    echo "<br>VALOR DE CIMA DAS MATRIZES SOMADAS";
+
+    
     
 
 ?>
