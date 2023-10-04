0036f11cfd8e735e6e87944d87dde11538d84cd7
joaoguilherme <joaoguilhermehig@gmail.com>
2023-10-04 19:34:05 -0300

    pegando a variavel do html

--- form/cadastro.php
+++ form/cadastro.php
@@ -2,4 +2,12 @@
 
     echo "<h1>Cadastrado!!!";
 
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+        $usuario = $_POST["usuario"];
+        $senha = $_POST["senha"];
+
+        echo "<h2> seu nome: $usuario";
+    }
+
+
 ?>
