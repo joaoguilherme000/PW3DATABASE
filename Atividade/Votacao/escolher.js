f731ef9be1aa6cbdeff473caa90ca811ed4269f9
joaoguilherme <joaoguilhermehig@gmail.com>
2023-11-01 20:00:27 -0300

    Escolha javascript feito

---
+++ Atividade/Votacao/escolher.js
@@ -0,0 +1,16 @@
+
+function mostrarTime (event) {
+    var idDoElementoClicado = event.target.id;
+    document.getElementById("demo").innerHTML = "Você escolheu: " + idDoElementoClicado;
+}
+
+var Corinthians = document.getElementById("Corinthians");
+var Palmeira = document.getElementById("Palmeira");
+var Santos = document.getElementById("Santos");
+var SPaulo = document.getElementById("Sao-Paulo");
+
+Corinthians.addEventListener("click", mostrarTime);
+Palmeira.addEventListener("click", mostrarTime);
+Santos.addEventListener("click", mostrarTime);
+SPaulo.addEventListener("click", mostrarTime);
+
