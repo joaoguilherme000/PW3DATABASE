faee6aadbe5cc0581be19b00babee54ad30198c4
joaoguilherme <joaoguilhermehig@gmail.com>
2023-11-06 19:22:59 -0300

    html junto php

--- Atividade/Votacao/Voto.php
+++ Atividade/Votacao/Voto.php
@@ -2,14 +2,24 @@
 if (isset($_COOKIE['voto'])) {
     $voto = $_COOKIE['voto'];
 
-    // Defina um cookie chamado "voto" com o valor $voto
     setcookie("voto", $voto, time() + 3600 * 24 * 31); // Expira em um mês
 
-    // Aqui, você pode usar a variável $voto conforme necessário.
-    // Por exemplo, para exibir o valor do voto:
-    echo "Você votou em: " . $voto;
+    $msg = $voto;
 } else {
     $msg = "Você não votou em nenhuma opção.";
     echo $msg;
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Voto</title>
+</head>
+<body>
+    <h1>Você votou em <?php  echo $voto;?></h1>
+</body>
+</html>
