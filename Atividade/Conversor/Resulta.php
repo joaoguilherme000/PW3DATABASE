6d4cb10ba56481b01c9874a7d8d67eb8cbddd736
joaoguilherme <joaoguilhermehig@gmail.com>
2023-11-13 20:14:48 -0300

    começando php

--- Atividade/Conversor/Resulta.php
+++ Atividade/Conversor/Resulta.php
@@ -1,25 +1,49 @@
-<?php
-if (isset($_COOKIE['voto'])) {
-    $voto = $_COOKIE['voto'];
-
-    setcookie("voto", $voto, time() + 3600 * 24 * 31); // Expira em um mês
-
-    $msg = "obrigado por votar";
-} else {
-    $msg = "Voce ja votou";
-}
-?>
-
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="dist/styles.css">
-    <title>Voto</title>
+    <link rel="stylesheet" href="./dist/styles.css">
+    <title>Calculadora Gorjeta</title>
 </head>
-<body class="content2">
-    <h1><?php  echo $msg;?></h1>
+<body>
+
+    <form name="calculadora" method="post" action="Resulta.php" class="janela">
+        <h1>Conversor de Unidades</h1>
+
+        <label for="quantidade" id="bold">Digite a quantidade: </label>
+
+        <?php 
+            $valor = $_POST["quantidade"];
+            echo '<input type="number" name="quantidade" class="quantidade" id="valor" min="0" max="9999.99" value="' . $valor . '">';
+        ?>
+    
+        <label for="selecao" id="bold">De:</label>
+
+        <select name="selecao" id="selecao">
+            <option>Metro(s)</option>
+            <option>Quilômetros(s)</option>
+            <option>Centímetros(s)</option>
+            <option>Milímetro(s)</option>
+        </select>
+
+        <label for="para" id="bold">Para:</label>
+
+        <select name="selecao" class="para">
+            <option>Metro(s)</option>
+            <option>Quilômetros(s)</option>
+            <option>Centímetros(s)</option>
+            <option>Milímetro(s)</option>
+        </select>
+
+        <input id="botao" type="submit" value="Converter">
+
+        <label id="bold">Resultado: </label>
+        <label id="mostrar">.</label>
+            
+        <label style="display: none;">x quilometro(s) equivalem a y Metros(s)</label>
+    </form>
+    <script src="./LengthInput.js"></script>
 </body>
 </html>
