c30b4a3aab63f4a68b86ba52e4625fed77b99444
joaoguilherme <joaoguilhermehig@gmail.com>
2024-03-04 22:13:29 -0300

    mostrando as informações do banco

--- meus/login/mostrar.php
+++ meus/login/mostrar.php
@@ -1,17 +1,18 @@
 <?php
 
-    $localhost = 'localhost';
-    $user_name = 'root';
-    $senha = "";
-    $db = "dbPessoa";
-
     if($_SERVER["REQUEST_METHOD"] == "POST"){
         $nome = $_POST["nome"];
         $sobrenome = $_POST["sobrenome"];
         $idade = $_POST["idade"];
     }
 
-    $con = mysqli_connect($localhost,$user_name,$senha,$db);
+  $localhost = 'localhost';
+	$user_name = 'root';
+	$senha = "";
+	$db = "dbPessoa";
+ 
+ 
+	$con = mysqli_connect($localhost,$user_name,$senha,$db);
 		if (mysqli_connect_errno()){
 			echo "Erro ao conectar com o banco de dados: " . mysqli_connect_error();
 		}else{
@@ -35,18 +36,43 @@
     <title>Formulário de Cadastro</title>
 </head>
 <body >
-    <div class="data">
-        <div class="header">
-            <h1>Suas informações de login</h1>
-        </div>
-        <div class="rowData">
-            <h1><?php echo $nome?></h1>
-            <h1><?php echo $sobrenome?></h1>
-            <h1><?php echo $idade?></h1>
-        </div>
-        <h1 class="send">
-            informação enviada para o banco de dados 👀
-        </h1>
+  <div class="data">
+    <div class="user">
+      <div class="rowData">
+        <h2>Suas informações de login</h2>
+        <h1><?php echo $nome?></h1>
+        <h1><?php echo $sobrenome?></h1>
+        <h1><?php echo $idade?></h1>
+      </div>
+      <h2 >
+          informação enviada para o banco de dados 👀
+      </h2>
     </div>
+    <div class="database">
+      <div class="tables">
+        <h1 class="tb">Nomes</h1>
+        <h1 class="tb">Sobrenomes</h1>
+        <h1 class="tb">Idades</h1>
+      </div>
+      <div class="information" >
+        <?php
+        $con = mysqli_connect($localhost, $user_name, $senha, $db);
+        if (mysqli_connect_errno()){
+            echo "Erro ao conectar com o banco de dados: " . mysqli_connect_error();
+        } else {
+            $mostrar = "SELECT nome, sobrenome, idade FROM tbPessoa LIMIT 5";
+            $resultado = mysqli_query($con, $mostrar);
+            while ($pessoa = mysqli_fetch_array($resultado)) {
+                echo "<div class='infos'>";
+                echo "<h1 class='info'>" . $pessoa['nome'] . "</h1>";
+                echo "<h1 class='info'>" . $pessoa['sobrenome'] . "</h1>";
+                echo "<h1 class='info'>" . $pessoa['idade'] . "</h1>";
+                echo "</div>";
+            }
+            mysqli_close($con);
+        }
+        ?>
+      </div>
+  </div>
 </body>
 </html>
