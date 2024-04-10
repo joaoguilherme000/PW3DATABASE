7fb18fd0b8f5167794319c170a94eea07766c1a6
joaoguilherme <joaoguilhermehig@gmail.com>
2024-04-09 22:17:25 -0300

    configurado

---
+++ Atividade/aut/database.sql
@@ -0,0 +1,24 @@
+drop database database;
+
+create database database;
+
+
+use database;
+
+CREATE TABLE tbPessoa(
+codigo int not null auto_increment,
+nome VARCHAR(100),
+idade INT,
+primary key(codigo));
+
+-- insert into tbPessoa(nome,sobrenome,idade)
+    -- values("Etec", "Irmã Agostina", 11);
+
+-- visualizar as tabelas criadas
+show tables;
+
+-- visualizando a estrutura das tabelas
+desc tbPessoa;
+
+-- conteudo das tabelas
+select * from tbPessoa;
